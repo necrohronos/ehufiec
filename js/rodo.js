b3a558a349ab9f9c128ce9d92480c46c4cca690c
Bogdan Ciechomski <bogdan.ciechomski@gmail.com>
2018-08-04 20:41:32 +0200

    dodanie informacji o RODO

---
+++ js/rodo.js
@@ -0,0 +1,6 @@
+let closeX = document.getElementById('wypierdalaj-mi-z-tym');
+let rodoDiv = document.getElementById('rodo-shadow')
+
+closeX.addEventListener('click', function() {
+    rodoDiv.style.display = 'none';
+})
